31256433d4f6444835a452a6a336ea0a83075519
decchi <idemasahiro@hotmail.co.jp>
2017-09-24 09:40:47 +0900

    add turn

---
+++ Assets/OrenoYome/Script/IKLookAt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKLookAt : MonoBehaviour
+{
+    private Animator avator;
+    public Transform lookAtObj = null;
+
+    [SerializeField, Range(0, 1)]
+    private float lookAtWeight = 1.0f;
+    [SerializeField, Range(0, 1)]
+    private float bodyWeight = 0.4f;
+    [SerializeField, Range(0, 1)]
+    private float headWeight = 1f;
+    [SerializeField, Range(0, 1)]
+    private float eyesWeight = 0f;
+    [SerializeField, Range(0, 1)]
+    private float clampWeight = 0.4f;
+
+    // Use this for initialization
+    void Start()
+    {
+        avator = GetComponent<Animator>();
+        if (lookAtObj == null)
+        {
+            lookAtObj = Camera.main.transform;
+        }
+    }
+
+    void OnAnimatorIK(int layorIndex)
+    {
+        if (avator)
+        {
+            avator.SetLookAtWeight(lookAtWeight, bodyWeight, headWeight, eyesWeight, clampWeight);
+            avator.SetLookAtPosition(lookAtObj.position);
+        }
+    }
+}
